a25358feeaeacd220f3982612fb0511849182c12
Derick S. Park <145691757+derickspark@users.noreply.github.com>
2025-07-20 09:49:52 +0900

    Delete zzz.R

--- zzz.R
+++
@@ -1,7 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  # RCurl을 강제로 로드 (패키지 attach 시 실행)
-  if (!"RCurl" %in% loadedNamespaces()) {
-    requireNamespace("RCurl", quietly = TRUE)
-    library(RCurl, character.only = TRUE)
-  }
-}
